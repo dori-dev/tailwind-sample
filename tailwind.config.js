7168287cf34f52155bc6d1ffbb4454307d4ec8dd
dori-dev <mr.dori.dev@gmail.com>
2023-07-07 20:56:42 +0330

    Add vazir font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      vazir: ["vazir", "sans-serif"],
+      vazirBold: ["vazirBold", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
